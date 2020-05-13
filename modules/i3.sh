f1a5e5e21aa6c966bc622f60f3b4bad608d8f449
Bobby Brockway <bobby.brockway@gmail.com>
2020-05-13 17:29:40 +0100

    consolidated i3 modules

--- modules/i3.sh
+++ modules/i3.sh
@@ -2,11 +2,17 @@
 
 export I3_PKGS=(
   arandr
+  blueman
   dmenu
   feh
   i3-gaps
   i3status
+  network-manager-applet
+  pavucontrol
   perl-anyevent-i3
+  polybar
+  udiskie
+  volctl
 )
 
 arch-chroot "$ARCH" pacman -Syu --needed --noconfirm "${I3_PKGS[@]}"
